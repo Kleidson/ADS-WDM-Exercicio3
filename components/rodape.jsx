a0d73c803d3fb720d6926aae621ae97703d44166
Kleidson <kleidson.ar@gmail.com>
2020-09-20 20:39:36 -0300

    Create rodape.jsx

---
+++ components/rodape.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rodape = () => {
+    return (
+        <div>
+            <hr/>
+            <p>Isto é um Rodapé</p>
+        </div>
+    )
+}
+
+export default Rodape
